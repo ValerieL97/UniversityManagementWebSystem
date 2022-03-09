ef73081b5ec588ef2d21f58bdd90b4374c03c37a
Valerie L <84472171+ValerieL97@users.noreply.github.com>
2022-03-09 16:48:55 +0300

    Update ClassService.java

--- src/main/java/com/myproject/UniversityManagementSystem/service/ClassService.java
+++ src/main/java/com/myproject/UniversityManagementSystem/service/ClassService.java
@@ -14,49 +14,38 @@ import java.util.Set;
 @Service
 public class ClassService {
 
-    private ClassRepo classRepo;
+    private final ClassRepo classRepo;
 
-    public ClassService(ClassRepo classRepo) {
+    public ClassService( ClassRepo classRepo) {
         this.classRepo = classRepo;
     }
 
-    public List<Classes> getAllClasses() {
-        return classRepo.findAll();
-    }
-
-    public Classes saveClass(Classes class1){
-        return classRepo.save(class1);
-    }
-
-    public Classes updateClass(Classes class1){
-        return classRepo.save(class1);
-    }
-
-    public Classes getClassById(Long id) {
-        return classRepo.getById(id);
-    }
-    public void deleteClassById(Long id){
-        classRepo.deleteById(id);
-    }
-
-    public Set<Classes> findByCourse(Course course) {
-        Set<Classes> courseClasses = new HashSet<>();
-        for(Classes class1 : classRepo.findAll()) {
-            if(class1.getCourse().equals(course)) {
-               courseClasses.add(class1);
-            }
+    public Classes saveClass(Classes class1) {
+        Classes class2 = classRepo.findByClassName(class1.getClassName());
+        if(class2 == null){
+            return classRepo.save(class1);
         }
 
-        return courseClasses;
+        return null;
     }
 
-    public boolean existingClass(Classes classes) {
-        List<Classes> classesList = getAllClasses();
-        for (Classes class1: classesList) {
-            if (class1.getClassName().equals(classes.getClassName())) {
-                return true;
-            }
-        }
-        return false;
+
+    public Classes getClassById(Long classId) {
+        return classRepo.findById(classId).get();
+    }
+
+    public void deleteClass(Long classId) {
+        classRepo.deleteById(classId);
+    }
+
+    public Classes updateClassInfo(Classes classes, Long classId) {
+        Classes class1 = getClassById(classId);
+        class1.setTeacher(classes.getTeacher());
+        class1.setId(classId);
+        class1.setCourse(classes.getCourse());
+        class1.setClassName(classes.getClassName());
+        class1.setDuration(classes.getDuration());
+        class1.setSemester(classes.getSemester());
+        return classRepo.save(class1);
     }
 }
