fb72de2ad610e41aabeab4506e1bbd1c755e1ac2
Valerie L <84472171+ValerieL97@users.noreply.github.com>
2022-03-09 16:47:51 +0300

    Update RegistrationController.java

--- src/main/java/com/myproject/UniversityManagementSystem/controller/RegistrationController.java
+++ src/main/java/com/myproject/UniversityManagementSystem/controller/RegistrationController.java
@@ -15,39 +15,28 @@ import java.util.Collections;
 @Controller
 public class RegistrationController {
 
-    private UserRepo userRepository;
     private UserService userService;
-    private PasswordEncoder passwordEncoder;
 
-    public RegistrationController(UserRepo userRepository, UserService userService,
-                                  PasswordEncoder passwordEncoder) {
-        this.userRepository = userRepository;
+    public RegistrationController(UserService userService) {
         this.userService = userService;
-        this.passwordEncoder = passwordEncoder;
     }
 
     @GetMapping("/registration")
     public String registration(Model model) {
         User user = new User();
-        model.addAttribute("users",user);
+        model.addAttribute("user",user);
         return "registration";
     }
 
 
     @RequestMapping(value="/registration", method = RequestMethod.POST)
-    public String addUser(@ModelAttribute("users")User user){
+    public String addUser(@ModelAttribute("user")User user){
 
-        if(!userService.addNewUser(user)) {
-            return "redirect:/registration?success1";
-        } else if(!user.getPasswordConf().equals(user.getPassword())) {
+        if(!user.getPasswordConf().equals(user.getPassword())) {
             return "redirect:/registration?success2";
+        }else if(!userService.saveUser(user)) {
+            return "redirect:/registration?success1";
         }
-
-        user.setAdmin(false);
-        user.setActive(true);
-        user.setPassword(passwordEncoder.encode(user.getPassword()));
-        user.setRoles(Collections.singleton(Role.STUDENT));
-        userRepository.save(user);
 
         return "redirect:/login";
     }
