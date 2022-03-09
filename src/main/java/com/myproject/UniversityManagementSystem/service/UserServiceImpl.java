b75660885e2993ca578fdc18e060dd43c6be347a
Valerie L <84472171+ValerieL97@users.noreply.github.com>
2022-03-09 16:57:48 +0300

    Add files via upload

---
+++ src/main/java/com/myproject/UniversityManagementSystem/service/UserServiceImpl.java
@@ -0,0 +1,25 @@
+package com.example.UniversityManagementSystem.service;
+
+import com.example.UniversityManagementSystem.models.User;
+import com.example.UniversityManagementSystem.repositories.UserRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserDetailsService {
+
+    @Autowired
+    private UserRepo userRepo;
+
+    public UserServiceImpl() {
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepo.findByUsername(username);
+        return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(), user.getRoles());
+    }
+}
