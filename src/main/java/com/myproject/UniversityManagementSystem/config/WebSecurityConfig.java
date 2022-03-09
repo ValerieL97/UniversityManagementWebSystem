cb11d1a456d748f456ba8162717781ad73f6b073
Valerie L <84472171+ValerieL97@users.noreply.github.com>
2022-03-09 17:00:11 +0300

    Update WebSecurityConfig.java

--- src/main/java/com/myproject/UniversityManagementSystem/config/WebSecurityConfig.java
+++ src/main/java/com/myproject/UniversityManagementSystem/config/WebSecurityConfig.java
@@ -1,7 +1,7 @@
 package com.myproject.UniversityManagementSystem.config;
 
 
-import com.myproject.UniversityManagementSystem.service.UserService;
+import com.myproject.UniversityManagementSystem.service.UserServiceImpl;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
