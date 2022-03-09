85c6cfcaff412d0b74cd2e25044ac2390bb52f96
Valerie L <84472171+ValerieL97@users.noreply.github.com>
2022-03-09 16:59:06 +0300

    Update UserServiceImpl.java

--- src/main/java/com/myproject/UniversityManagementSystem/service/UserServiceImpl.java
+++ src/main/java/com/myproject/UniversityManagementSystem/service/UserServiceImpl.java
@@ -1,7 +1,7 @@
-package com.example.UniversityManagementSystem.service;
+package com.myproject.UniversityManagementSystem.service;
 
-import com.example.UniversityManagementSystem.models.User;
-import com.example.UniversityManagementSystem.repositories.UserRepo;
+import com.myproject.UniversityManagementSystem.models.User;
+import com.myproject.UniversityManagementSystem.repositories.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
