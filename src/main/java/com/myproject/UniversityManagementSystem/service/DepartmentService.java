7b9fb484346ef604be2837d9d756b84fdcef5379
Valerie L <84472171+ValerieL97@users.noreply.github.com>
2022-03-09 16:50:46 +0300

    Update DepartmentService.java

--- src/main/java/com/myproject/UniversityManagementSystem/service/DepartmentService.java
+++ src/main/java/com/myproject/UniversityManagementSystem/service/DepartmentService.java
@@ -16,84 +16,50 @@ import java.util.Set;
 @Service
 public class DepartmentService {
 
-   private DepartmentRepo departmentRepo;
-   private CourseService courseService;
-   private TeacherService teacherService;
+       private final DepartmentRepo departmentRepo;
 
-    public DepartmentService(DepartmentRepo departmentRepo,
-                             CourseService courseService,
-                             TeacherService teacherService) {
+    public DepartmentService(DepartmentRepo departmentRepo) {
         this.departmentRepo = departmentRepo;
-        this.courseService = courseService;
-        this.teacherService = teacherService;
+    }
+
+    public List<Department> getAllDepartments() {
+        return departmentRepo.findAll();
+
+    }
+
+    public int calculateNumberOfStudents(Department department) {
+        List<Course> courses = department.getCourses();
+        int studentsNum = 0;
+
+        for(Course course : courses) {
+            studentsNum = studentsNum + course.getStudents().size();
+        }
+
+        return studentsNum;
+    }
+
+    public boolean existingDepartment(String departmentName) {
+        Department department = departmentRepo.findByDepartmentName(departmentName);
+        if(department != null) {
+            return true;
+        }
+        return false;
+    }
+
+    public Department saveDepartment(Department department) {
+        return departmentRepo.save(department);
     }
 
     public Department getDepartmentById(Long id) {
-
-        return departmentRepo.getById(id);
+        return departmentRepo.findById(id).get();
     }
-    public List<Department> getAllDepartments() {
-        return departmentRepo.findAll();
-    }
-    public Department saveDepartment(Department department){
 
+    public Department updateDepartment(Department department) {
         return departmentRepo.save(department);
     }
-    public Department updateDepartment(Department department){
 
-        return departmentRepo.save(department);
-    }
     public void deleteDepartment(Long id) {
-
         departmentRepo.deleteById(id);
-    }
-
-    public Department getDepartmentByName(String departmentName) {
-        List<Department> departments = getAllDepartments();
-        Department department = null;
-
-        for(Department dep : departments) {
-            if(dep.getDepartmentName() == departmentName) {
-                department =  dep;
-            }
-        }
-
-        return department;
-
-    }
-
-
-
-    public int countNumberCourses(Long id) {
-        Set<Course> courses = courseService.findByDepartment(getDepartmentById(id));
-        return courses.size();
-    }
-
-    public int countNumberTeachers(Long id) {
-        Set<Teacher> teachers = teacherService.findByDepartment(getDepartmentById(id));
-        return teachers.size();
-    }
-
-    public int countNumberStudents(Long id) {
-        Set<Course> courses = courseService.findByDepartment(getDepartmentById(id));
-        int numStudents = 0;
-
-        for(Course course: courses) {
-            numStudents = numStudents + courseService.countNumStudents(course.getCourseId());
-        }
-
-        return numStudents;
-
-    }
-
-    public boolean existingDepartment(String name) {
-        List<Department> departments = getAllDepartments();
-        for (Department department1 : departments) {
-            if (department1.getDepartmentName().equals(name)) {
-                return true;
-            }
-        }
-        return false;
     }
 
 
